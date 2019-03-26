478a9ed3ffb37d87f94d66fa272a051d95404ee4
Max Anurin <maxim.anurin@zxteam.org>
2019-03-26 14:13:47 +0200

    Switch to 3.0 contract

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import * as http from "http";
 import * as https from "https";
 import { URL } from "url";
 
-import { LoggerLike, CancellationTokenLike, InvokeTransportLike } from "@zxteam/contract";
+import { Logger, CancellationToken, InvokeTransport, Task as TaskLike } from "@zxteam/contract";
 import { Disposable } from "@zxteam/disposable";
 import { loggerFactory } from "@zxteam/logger";
+import { Task } from "ptask.js";
 
 export interface WebClientInvokeArgs {
 	url: URL;
@@ -19,12 +20,12 @@ export interface WebClientInvokeResult {
 	body: Buffer;
 }
 
-export type WebClientLike = InvokeTransportLike<WebClientInvokeArgs, WebClientInvokeResult>;
+export type WebClientLike = InvokeTransport<WebClientInvokeArgs, WebClientInvokeResult>;
 
 export class WebClient extends Disposable implements WebClientLike {
 	private readonly _proxyOpts: WebClient.ProxyOpts | null;
 	private readonly _sslOpts: WebClient.SslOpts | null;
-	private _log: LoggerLike;
+	private _log: Logger;
 	private _requestTimeout: number | null;
 	public constructor(opts?: WebClient.Opts) {
 		super();
@@ -34,144 +35,65 @@ export class WebClient extends Disposable implements WebClientLike {
 	}
 
 	public get log() { return this._log || (this._log = loggerFactory.getLogger(this.constructor.name)); }
-	public set log(value: LoggerLike) { this._log = value; }
+	public set log(value: Logger) { this._log = value; }
 
 	public invoke(
 		{ url, method, headers, body }: WebClientInvokeArgs,
-		cancellationToken?: CancellationTokenLike
-	): Promise<WebClientInvokeResult> {
+		cancellationToken?: CancellationToken
+	): TaskLike<WebClientInvokeResult> {
 		super.verifyNotDisposed();
+		return Task.run(() => {
+			if (this.log.isTraceEnabled) { this.log.trace("begin invoke(...)", url, method, headers, body); }
+			return new Promise<WebClientInvokeResult>((resolve, reject) => {
+				const responseHandler = (response: http.IncomingMessage) => {
+					const responseDataChunks: Array<Buffer> = [];
+					response.on("data", (chunk: Buffer) => responseDataChunks.push(chunk));
+					response.on("error", error => reject(error));
+					response.on("end", () => {
+						const respStatus = response.statusCode || 500;
+						const respDescription = response.statusMessage || "";
+						const respHeaders = response.headers;
+						const respBody = Buffer.concat(responseDataChunks);
 
-		if (this.log.isTraceEnabled) { this.log.trace("begin invoke(...)", url, method, headers, body); }
-		return new Promise<WebClientInvokeResult>((resolve, reject) => {
-			const responseHandler = (response: http.IncomingMessage) => {
-				const responseDataChunks: Array<Buffer> = [];
-				response.on("data", (chunk: Buffer) => responseDataChunks.push(chunk));
-				response.on("error", error => reject(error));
-				response.on("end", () => {
-					const respStatus = response.statusCode || 500;
-					const respDescription = response.statusMessage || "";
-					const respHeaders = response.headers;
-					const respBody = Buffer.concat(responseDataChunks);
-
-					if (respStatus < 400) {
-						return resolve({
-							statusCode: respStatus, statusMessage: respDescription,
-							headers: respHeaders, body: respBody
-						});
-					} else {
-						return reject(new WebClient.WebError(respStatus, respDescription, respHeaders, respBody));
-					}
-				});
-			};
-			if (cancellationToken) { cancellationToken.throwIfCancellationRequested(); }
-			function registerCancelOperationIfNeeded(requestLike: { abort: () => void }) {
-				if (cancellationToken) {
-					const cb = () => {
-						cancellationToken.removeCancelListener(cb);
-						requestLike.abort();
-						try {
-							cancellationToken.throwIfCancellationRequested(); // Shoud raise error
-							// Guard for broken implementation of cancellationToken
-							reject(new Error("Cancelled by user"));
-						} catch (e) {
-							reject(e);
-						}
-					};
-					cancellationToken.addCancelListener(cb);
-				}
-			}
-
-			let isConnecTimeout: boolean = false;
-			const proxyOpts = this._proxyOpts;
-			if (proxyOpts && proxyOpts.type === "http") {
-				const reqOpts = {
-					protocol: "http:",
-					host: proxyOpts.host,
-					port: proxyOpts.port,
-					path: url.href,
-					method,
-					headers: { Host: url.host, ...headers }
-				};
-				if (this.log.isTraceEnabled) { this.log.trace("Call http.request", reqOpts); }
-				const request = http.request(reqOpts, responseHandler)
-					.on("error", error => {
-						const msg = isConnecTimeout ? "Connect Timeout" : "http.request failed. See innderError for details";
-						this.log.debug(msg, error);
-						reject(new WebClient.CommunicationError(msg, error));
-					});
-				if (this._requestTimeout !== null) {
-					request.setTimeout(this._requestTimeout, () => {
-						request.abort();
-						isConnecTimeout = true;
-					});
-					request.on("socket", socket => {
-						socket.setTimeout(this._requestTimeout);
-						socket.on("timeout", () => {
-							request.abort();
-							isConnecTimeout = true;
-						});
-					});
-				}
-				if (body) {
-					if (this.log.isTraceEnabled) { this.log.trace("write body", body.toString()); }
-					request.write(body);
-				}
-				request.end();
-				registerCancelOperationIfNeeded(request);
-			} else {
-				const reqOpts: https.RequestOptions = {
-					protocol: url.protocol,
-					host: url.hostname,
-					port: url.port,
-					path: url.pathname + url.search,
-					method: method,
-					headers: headers
-				};
-				if (reqOpts.protocol === "https:") {
-					const sslOpts = this._sslOpts;
-					if (sslOpts) {
-						if (sslOpts.ca) {
-							reqOpts.ca = sslOpts.ca;
-						}
-						if (sslOpts.rejectUnauthorized !== undefined) {
-							reqOpts.rejectUnauthorized = sslOpts.rejectUnauthorized;
-						}
-						if ("pfx" in sslOpts) {
-							reqOpts.pfx = sslOpts.pfx;
-							reqOpts.passphrase = sslOpts.passphrase;
-						} else if ("cert" in sslOpts) {
-							reqOpts.key = sslOpts.key;
-							reqOpts.cert = sslOpts.cert;
-						}
-					}
-					if (this.log.isTraceEnabled) { this.log.trace("Call https.request", reqOpts); }
-					const request = https.request(reqOpts, responseHandler)
-						.on("error", error => {
-							const msg = isConnecTimeout ? "Connect Timeout" : "http.request failed. See innderError for details";
-							this.log.debug(msg, error);
-							reject(new WebClient.CommunicationError(msg, error));
-						});
-					if (this._requestTimeout !== null) {
-						request.setTimeout(this._requestTimeout, () => {
-							request.abort();
-							isConnecTimeout = true;
-						});
-						request.on("socket", socket => {
-							socket.setTimeout(this._requestTimeout);
-							socket.on("timeout", () => {
-								request.abort();
-								isConnecTimeout = true;
+						if (respStatus < 400) {
+							return resolve({
+								statusCode: respStatus, statusMessage: respDescription,
+								headers: respHeaders, body: respBody
 							});
-						});
+						} else {
+							return reject(new WebClient.WebError(respStatus, respDescription, respHeaders, respBody));
+						}
+					});
+				};
+				if (cancellationToken !== undefined) { cancellationToken.throwIfCancellationRequested(); }
+				function registerCancelOperationIfNeeded(requestLike: { abort: () => void }) {
+					if (cancellationToken !== undefined) {
+						const cb = () => {
+							cancellationToken.removeCancelListener(cb);
+							requestLike.abort();
+							try {
+								cancellationToken.throwIfCancellationRequested(); // Shoud raise error
+								// Guard for broken implementation of cancellationToken
+								reject(new Error("Cancelled by user"));
+							} catch (e) {
+								reject(e);
+							}
+						};
+						cancellationToken.addCancelListener(cb);
 					}
-					if (body) {
-						if (this.log.isTraceEnabled) { this.log.trace("Write body", body.toString()); }
-						request.write(body);
-					}
-					request.end();
-					registerCancelOperationIfNeeded(request);
-				} else {
+				}
+
+				let isConnecTimeout: boolean = false;
+				const proxyOpts = this._proxyOpts;
+				if (proxyOpts && proxyOpts.type === "http") {
+					const reqOpts = {
+						protocol: "http:",
+						host: proxyOpts.host,
+						port: proxyOpts.port,
+						path: url.href,
+						method,
+						headers: { Host: url.host, ...headers }
+					};
 					if (this.log.isTraceEnabled) { this.log.trace("Call http.request", reqOpts); }
 					const request = http.request(reqOpts, responseHandler)
 						.on("error", error => {
@@ -193,13 +115,93 @@ export class WebClient extends Disposable implements WebClientLike {
 						});
 					}
 					if (body) {
-						if (this.log.isTraceEnabled) { this.log.trace("Write body", body.toString()); }
+						if (this.log.isTraceEnabled) { this.log.trace("write body", body.toString()); }
 						request.write(body);
 					}
 					request.end();
 					registerCancelOperationIfNeeded(request);
+				} else {
+					const reqOpts: https.RequestOptions = {
+						protocol: url.protocol,
+						host: url.hostname,
+						port: url.port,
+						path: url.pathname + url.search,
+						method: method,
+						headers: headers
+					};
+					if (reqOpts.protocol === "https:") {
+						const sslOpts = this._sslOpts;
+						if (sslOpts) {
+							if (sslOpts.ca) {
+								reqOpts.ca = sslOpts.ca;
+							}
+							if (sslOpts.rejectUnauthorized !== undefined) {
+								reqOpts.rejectUnauthorized = sslOpts.rejectUnauthorized;
+							}
+							if ("pfx" in sslOpts) {
+								reqOpts.pfx = sslOpts.pfx;
+								reqOpts.passphrase = sslOpts.passphrase;
+							} else if ("cert" in sslOpts) {
+								reqOpts.key = sslOpts.key;
+								reqOpts.cert = sslOpts.cert;
+							}
+						}
+						if (this.log.isTraceEnabled) { this.log.trace("Call https.request", reqOpts); }
+						const request = https.request(reqOpts, responseHandler)
+							.on("error", error => {
+								const msg = isConnecTimeout ? "Connect Timeout" : "http.request failed. See innderError for details";
+								this.log.debug(msg, error);
+								reject(new WebClient.CommunicationError(msg, error));
+							});
+						if (this._requestTimeout !== null) {
+							request.setTimeout(this._requestTimeout, () => {
+								request.abort();
+								isConnecTimeout = true;
+							});
+							request.on("socket", socket => {
+								socket.setTimeout(this._requestTimeout);
+								socket.on("timeout", () => {
+									request.abort();
+									isConnecTimeout = true;
+								});
+							});
+						}
+						if (body) {
+							if (this.log.isTraceEnabled) { this.log.trace("Write body", body.toString()); }
+							request.write(body);
+						}
+						request.end();
+						registerCancelOperationIfNeeded(request);
+					} else {
+						if (this.log.isTraceEnabled) { this.log.trace("Call http.request", reqOpts); }
+						const request = http.request(reqOpts, responseHandler)
+							.on("error", error => {
+								const msg = isConnecTimeout ? "Connect Timeout" : "http.request failed. See innderError for details";
+								this.log.debug(msg, error);
+								reject(new WebClient.CommunicationError(msg, error));
+							});
+						if (this._requestTimeout !== null) {
+							request.setTimeout(this._requestTimeout, () => {
+								request.abort();
+								isConnecTimeout = true;
+							});
+							request.on("socket", socket => {
+								socket.setTimeout(this._requestTimeout);
+								socket.on("timeout", () => {
+									request.abort();
+									isConnecTimeout = true;
+								});
+							});
+						}
+						if (body) {
+							if (this.log.isTraceEnabled) { this.log.trace("Write body", body.toString()); }
+							request.write(body);
+						}
+						request.end();
+						registerCancelOperationIfNeeded(request);
+					}
 				}
-			}
+			});
 		});
 	}
 
