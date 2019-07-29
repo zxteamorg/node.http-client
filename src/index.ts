409d366b1b7f3eaa417b5f3bbcd6d9b6ba3f769d
Max Anurin <maxim.anurin@zxteam.org>
2019-07-29 21:07:59 +0300

    Update conflict guard

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-const { name, version } = require(require("path").join(__dirname, "..", "package.json"));
+const { name: packageName, version: packageVersion } = require(require("path").join(__dirname, "..", "package.json"));
 const G: any = global || window || {};
-const PACKAGE_GUARD: symbol = Symbol.for(name);
+const PACKAGE_GUARD: symbol = Symbol.for(packageName);
 if (PACKAGE_GUARD in G) {
 	const conflictVersion = G[PACKAGE_GUARD];
 	// tslint:disable-next-line: max-line-length
-	const msg = `Conflict module version. Look like two different version of package ${name} was loaded inside the process: ${conflictVersion} and ${version}.`;
+	const msg = `Conflict module version. Look like two different version of package ${packageName} was loaded inside the process: ${conflictVersion} and ${packageVersion}.`;
 	if (process !== undefined && process.env !== undefined && process.env.NODE_ALLOW_CONFLICT_MODULES === "1") {
 		console.warn(msg + " This treats as warning because NODE_ALLOW_CONFLICT_MODULES is set.");
 	} else {
 		throw new Error(msg + " Use NODE_ALLOW_CONFLICT_MODULES=\"1\" to treats this error as warning.");
 	}
 } else {
-	G[PACKAGE_GUARD] = version;
+	G[PACKAGE_GUARD] = packageVersion;
 }
 
 import * as zxteam from "@zxteam/contract";
